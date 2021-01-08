94ec87ada1f3cb452b90afd070c7d8fc22ea8104
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-01-08 18:30:28 +0800

    cart component total order modified

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -6,7 +6,8 @@ function Cart(props) {
   let total = 0;
   for (let i = 0; i < cart.length; i++) {
     const product = cart[i];
-    total = total + product.price;
+    total = (total + product.price);
+    total.toFixed(2)
   }
   let shipping = 0;
   if (total > 50) {
@@ -16,15 +17,22 @@ function Cart(props) {
   } else if (total > 0) {
     shipping = 12.99;
   }
+  // shipping.toFixed(2)
   const tax = (total / 10).toFixed(2);
+  const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+  const formateNumber = num => {
+    const precision = num.toFixed(2);
+    return Number(precision)
+  }
   return (
     <div>
       <h4>Order Summary</h4>
       <p>Items Order: {cart.length}</p>
-      <p>Product Price: {total}</p>
+      <p>Product Price: {formateNumber(total)}</p>
       <p>Shipping cost: {shipping}</p>
       <p>Tax: {tax}</p>
-      <p>Total Price: {total + shipping + Number(tax)}</p>
+      <p>Total Price: {grandTotal}</p>
+      {/* use Number string to num */}
     </div>
   );
 }
